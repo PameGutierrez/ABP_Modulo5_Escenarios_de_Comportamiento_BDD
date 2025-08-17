15cad7eef3dc171fbe303088634e150b84a76643
Pamela Gutiérrez <ptgutierrezgm@gmail.com>
2025-08-17 13:35:35 -0400

    con generacion de reporte

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,6 +1,7 @@
 package runner;
 
 import org.junit.runner.RunWith;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
@@ -8,7 +9,14 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
     features = "src/test/resources/features",
     glue = {"steps", "hooks"},
-    plugin = {"pretty", "html:target/cucumber-report.html", "json:target/cucumber.json"},
+    plugin = {
+        "pretty",
+        "html:target/cucumber-report.html",
+        // JSON 
+        "json:target/cucumber-reports/cucumber.json",
+        // Reporte avanzado Extent (se genera solo al terminar los tests)
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+    },
     monochrome = true
 )
 public class TestRunner { }
